7afcf0f380ea8b59d3ab11044b0850f997e07ead
Paul Fryzel <paul.fryzel@gmail.com>
2014-08-01 11:05:22 -0400

    Add identity filter and relevant example

---
+++ examples/response-filter.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var identity = require('..').filter.identity;
+var request = require('..').http.request;
+var response = require('..').http.response;
+
+var options = {
+  hostname: 'www.google.com',
+  headers: {
+    'content-type': 'text/html; charset=utf-8'
+  }
+};
+
+request(options).then(identity).then(response).done(function(buffer) {
+  console.log(buffer.toString().length);
+});
+
+request('http://www.google.com')
+  .then(identity)
+  .then(response)
+  .done(function(buffer) {
+    console.log(buffer.toString().length);
+  });
--- examples/run-all.js
+++ examples/run-all.js
@@ -2,4 +2,5 @@
 
 require('./http-request');
 require('./https-request');
+require('./response-filter');
 require('./json-parse');
