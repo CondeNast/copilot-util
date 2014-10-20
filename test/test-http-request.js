60a05cc3f151767f49a8d1a11c7e41261b5aec30
Paul Fryzel <paul.fryzel@gmail.com>
2014-10-20 18:28:35 -0400

    Append path to HTTP req/res errors

--- test/test-http-request.js
+++ test/test-http-request.js
@@ -7,22 +7,28 @@ var request = require('..').http.request;
 var sinon = require('sinon');
 
 describe('HTTP', function() {
-  var str = '渚 カヲル Nagisa Kaoru';
+  var buf = new Buffer('渚 カヲル Nagisa Kaoru');
 
   var setTimeoutSpy = sinon.spy();
   var endSpy = sinon.spy();
 
-  var requestStub = function() {
-    return {
-      setTimeout: setTimeoutSpy,
-      on: function(event, listener) {
-        if (event === 'response') {
-          listener.call(null, str);
+  var requestStub = function(options) {
+    return function() {
+      options = options || {};
+      return {
+        setTimeout: setTimeoutSpy,
+        on: function(event, listener) {
+          if (options.error && event === 'error') {
+            listener.call(null, new Error('boom'));
+          }
+          if (event === 'response') {
+            listener.call(null, buf);
+          }
+          return {
+            end: endSpy
+          };
         }
-        return {
-          end: endSpy
-        };
-      }
+      };
     };
   };
 
@@ -33,7 +39,7 @@ describe('HTTP', function() {
 
   describe('Request (HTTP)', function() {
     it('should respond with the data mocked into the listener', function(done) {
-      var stub = sinon.stub(http, 'request', requestStub);
+      var stub = sinon.stub(http, 'request', requestStub());
       var url = 'http://www.condenast.com';
 
       request(url).done(function(data) {
@@ -46,7 +52,7 @@ describe('HTTP', function() {
         expect(setTimeoutSpy.calledWith(15000)).to.eql(true);
         expect(endSpy.calledOnce).to.eql(true);
 
-        expect(data).to.eql(str);
+        expect(data.toString()).to.eql(buf.toString());
 
         stub.restore();
         done();
@@ -56,7 +62,7 @@ describe('HTTP', function() {
 
   describe('Secure Request (HTTPS)', function() {
     it('should respond with the data mocked into the listener', function(done) {
-      var stub = sinon.stub(https, 'request', requestStub);
+      var stub = sinon.stub(https, 'request', requestStub());
       var url = 'https://www.condenast.com';
 
       request(url).done(function(data) {
@@ -69,7 +75,30 @@ describe('HTTP', function() {
         expect(setTimeoutSpy.calledWith(15000)).to.eql(true);
         expect(endSpy.calledOnce).to.eql(true);
 
-        expect(data).to.eql(str);
+        expect(data.toString()).to.eql(buf.toString());
+
+        stub.restore();
+        done();
+      });
+    });
+  });
+
+  describe('Request (HTTP) HttpError', function() {
+    it('should respond with the data mocked into the listener', function(done) {
+      var stub = sinon.stub(http, 'request', requestStub({ error: true }));
+      var url = 'http://www.condenast.com';
+
+      request(url).done(null, function(err) {
+        var args = stub.args[0][0];
+        expect(args.protocol).to.eql('http:');
+        expect(args.href).to.eql(url + '/');
+        expect(args.agent.maxSockets).to.eql(Infinity);
+
+        expect(setTimeoutSpy.calledOnce).to.eql(true);
+        expect(setTimeoutSpy.calledWith(15000)).to.eql(true);
+        expect(endSpy.calledOnce).to.eql(true);
+
+        expect(err.message).to.contain('boom');
 
         stub.restore();
         done();
